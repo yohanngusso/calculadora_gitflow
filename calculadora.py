15fef4ea6f33dede70b5940f8742690906ef3893
yohanngusso <yohann.gusso@hotmail.com>
2025-09-22 04:47:38 -0300

    chore: initial commit

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def somar(a, b):
+    """Esta função soma dois números."""
+    return a + b
+
+if __name__ == "__main__":
+    print("Calculadora Simples")
+    resultado = somar(5, 3)
+    print(f"Resultado da soma: {resultado}")
